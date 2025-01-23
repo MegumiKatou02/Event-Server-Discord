adaf95ba7125425086da805d5a3b0b563e7e339a
ledinhchinh0212 <khongbachofficial@gmail.com>
2025-01-23 09:25:58 +0700

    Update error icon in Error.vue for improved clarity and replace image source

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -2,7 +2,7 @@
   <div class='error-page'>
     <div class="error-container" :class="whichTheme">
       <div class="error-icon">
-        <img src="/images/test.png" alt="">
+        <img src="/images/error-image.png" alt="">
       </div>
       <h1>Oops! Có lỗi xảy ra</h1>
       <p class="error-message">{{ errorMessage }}</p>
